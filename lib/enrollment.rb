295688413ba230fc8deeaf5d28ced84b7f536a6c
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2016-09-12 17:55:32 -0600

    Add enrollment intialize, add test

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -1,2 +1,7 @@
 class Enrollment
+  attr_reader :name, :kindergarten_participation
+  def initialize(hash)
+    @name = hash[:name]
+    @kindergarten_participation = hash[:kindergarten_participation]
+  end
 end
--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -2,5 +2,9 @@ require_relative 'test_helper'
 require_relative '../lib/enrollment'
 
 class EnrollmentTest < Minitest::Test
-  
+  def test_takes_data
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal "ACADEMY 20", e.name
+  end
 end
