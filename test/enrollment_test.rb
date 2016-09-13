a6f8787b515dae26ffa7fc72a1b0fc5ada9ccaae
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2016-09-12 19:59:40 -0600

    Add tests
    test it has a name
    test can be created

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -2,9 +2,24 @@ require_relative 'test_helper'
 require_relative '../lib/enrollment'
 
 class EnrollmentTest < Minitest::Test
-  def test_takes_data
+  def test_enrollment_can_be_created
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal Enrollment, e.class
+  end
+
+  def test_it_has_a_name
     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
     assert_equal "ACADEMY 20", e.name
   end
+
+  # def test_it_has_kindergarten_participation
+  #   e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+  #
+  #   #
+  # end
 end
+
+# test_it_returns_kindergarten_participation_by_year
+# test_it_returns_kindergarten_participation_in_a_given_year
