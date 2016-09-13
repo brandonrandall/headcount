0a5047d2b6eca7fb80b80bc7092fb0fdc4863fa3
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2016-09-12 20:11:30 -0600

    change participation test to match spec

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -14,12 +14,12 @@ class EnrollmentTest < Minitest::Test
     assert_equal "ACADEMY 20", e.name
   end
 
-  def test_it_has_kindergarten_participation
+  def test_it_has_kindergarten_participation_by_year
     e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
-    assert_equal ({2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}), e.kindergarten_participation
+    assert_equal ({2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}), e.kindergarten_participation_by_year 
   end
+
 end
 
-# test_it_returns_kindergarten_participation_by_year
 # test_it_returns_kindergarten_participation_in_a_given_year
