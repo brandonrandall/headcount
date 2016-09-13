b8d2ae4df54ecf6e92bce2c093423991cd57c04c
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2016-09-12 20:03:46 -0600

    add kinder participation test

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -14,11 +14,11 @@ class EnrollmentTest < Minitest::Test
     assert_equal "ACADEMY 20", e.name
   end
 
-  # def test_it_has_kindergarten_participation
-  #   e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
-  #
-  #   #
-  # end
+  def test_it_has_kindergarten_participation
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    assert_equal ({2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}), e.kindergarten_participation
+  end
 end
 
 # test_it_returns_kindergarten_participation_by_year
