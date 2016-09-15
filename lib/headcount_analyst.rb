bfca4f61ee7d523cdd95bb33a681cf143b582caa
Lee Saville <l.w.saville@gmail.com>
2016-09-15 17:00:22 -0600

    Finish kindergarten participation rate variation trend with supporting tests

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,6 +1,8 @@
 require  'pry'
+require_relative 'clean'
 
 class HeadcountAnalyst
+  include Clean
   attr_reader :district_repository, :calculate
   def initialize(dr)
     @district_repository = dr
@@ -10,18 +12,22 @@ class HeadcountAnalyst
     numerator = calculate(name)
     denominator = calculate(against[:against])
     variation = (numerator / denominator).round(3)
-
   end
 
   def kindergarten_participation_rate_variation_trend(name, against)
-    a = @district_repository.districts[name].enrollment
-    name_comparison = against[:against]
-    a.kindergarten_participation_in_year(name_comparison)
-    Clean.three_truncate(average)
-    numerator =
-    denominator =
-    trend_variation = (numerator / denominator).round(3)
+    numerator = @district_repository.find_by_name(name).enrollment.kindergarten_participation
+    denominator = @district_repository.find_by_name(against[:against]).enrollment.kindergarten_participation
+    variation_trend_calculator(numerator, denominator)
   end
+
+  def variation_trend_calculator(numerator, denominator)
+    result = {}
+    numerator.each do |key,value|
+      result[key] = Clean.three_truncate(numerator[key]/denominator[key])
+    end
+    result
+  end
+
   def calculate(name)
     years = @district_repository.find_by_name(name).enrollment.kindergarten_participation
     sum = years.reduce(0) do |sum, (key,value)|
@@ -29,10 +35,4 @@ class HeadcountAnalyst
     end
     average = Clean.three_truncate(sum / years.count)
   end
-
-
-  # def clean(percentage)
-  #   percentage.to_s[0..4].to_f
-  # end
-
 end
