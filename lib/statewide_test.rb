a96419d1d4dfbea636bf01157e0104af0b9284ef
Lee Saville <l.w.saville@gmail.com>
2016-09-18 19:07:18 -0600

    Add remaining statewide_test methods

--- lib/statewide_test.rb
+++ lib/statewide_test.rb
@@ -7,16 +7,52 @@ class StatewideTest
     @race_ethnicity_data = {}
   end
 
-  # def proficient_by_grade(grade)
-  # end
-  #
-  # def proficient_by_race_or_ethnicity(race)
-  # end
-  #
-  # def proficient_for_subject_by_grade_in_year(subject, grade, year)
-  # end
-  #
-  # def proficient_for_subject_by_race_in_year
-  # end
+  def proficient_by_grade(grade)
+    # require "pry"; binding.pry
+    if grade == 3
+      third_grade_polish
+    elsif grade == 8
+      eighth_grade_polish
+    end
+  end
+
+  def third_grade_polish
+    third_grade = @third_grade.each do |year, data|
+      data.each do |subject, percentage|
+        data[subject] = Clean.three_truncate(percentage)
+      end
+    end
+    third_grade
+  end
+
+  def eighth_grade_polish
+    eighth_grade = @eighth_grade.each do |year, data|
+      data.each do |subject, percentage|
+        data[subject] = Clean.three_truncate(percentage)
+      end
+    end
+    eighth_grade
+  end
+
+  def proficient_by_race_or_ethnicity(race)
+    race_data = @race_ethnicity_data[race].each do |year, data|
+      data.each do |subject, percentage|
+        data[subject] = Clean.three_truncate(percentage)
+      end
+    end
+    race_data
+  end
+
+  def proficient_for_subject_by_grade_in_year(subject, grade, year)
+    if grade == 3
+      Clean.three_truncate(@third_grade[year][subject])
+    elsif grade == 8
+      Clean.three_truncate(@eighth_grade[year][subject])
+    end
+  end
+
+  def proficient_for_subject_by_race_in_year(subject, race, year)
+    Clean.three_truncate(@race_ethnicity_data[race][year][subject])
+  end
 
 end
