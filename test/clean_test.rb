d396431958247f10e9d7f28cbf09850ad5bdd1bc
Lee Saville <l.w.saville@gmail.com>
2016-09-19 11:14:37 -0600

    pass iteration three. setup blank files for iteration four

--- test/clean_test.rb
+++ test/clean_test.rb
@@ -15,6 +15,7 @@ class CleanerTest < Minitest::Test
   def test_three_round
     assert_equal 0.988, Clean.three_round(0.987654)
     refute_equal 0.987, Clean.three_round(0.9877442)
+    assert_equal "N/A", Clean.three_round("N/A")
   end
 
   def test_race_ethnicity
@@ -22,6 +23,12 @@ class CleanerTest < Minitest::Test
     assert_equal :all_students, Clean.race_ethnicity("All Students")
     assert_equal :two_or_more, Clean.race_ethnicity("Two or More")
     assert_equal :asian, Clean.race_ethnicity("Asian")
+  end
+
+  def test_percentage
+    assert_equal "N/A", Clean.percentage("N/A")
+    assert_equal "N/A", Clean.percentage("LNE")
+    assert_equal 0.457, Clean.percentage("0.457")
   end
 
 end
