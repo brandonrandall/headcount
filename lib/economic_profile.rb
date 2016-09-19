ece391b385c54cac5c33c541d81190b5f87e3955
Lee Saville <l.w.saville@gmail.com>
2016-09-19 13:13:22 -0600

    Add some expected instance variables to the economic profile object

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -1,3 +1,27 @@
 class EconomicProfile
-  
+  def initialize(data)
+    @median_household_income = data[:median_household_income] || {}
+    @children_in_poverty = data[:children_in_poverty] || {}
+    @free_or_reduced_price_lunch = data[:free_or_reduced_price_lunch] || {}
+    @title_i = data[:title_i] || {}
+  end
+
+  # def median_household_income_in_year(year)
+  # end
+  #
+  # def median_household_income_average
+  # end
+  #
+  # def children_in_poverty_in_year(year)
+  # end
+  #
+  # def free_or_reduced_price_lunch_percentage_in_year(year)
+  # end
+  #
+  # def free_or_reduced_price_lunch_number_in_year(year)
+  # end
+  #
+  # def title_i_in_year(year)
+  # end
+
 end
