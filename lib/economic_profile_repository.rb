33431a302ddb81c907e0ca9b936dc655b552355b
Lee Saville <l.w.saville@gmail.com>
2016-09-19 13:12:01 -0600

    Add load_data method and similar control structure contents_control

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,3 +1,30 @@
+require_relative 'clean'
+require_relative 'data_extractor'
+
 class EconomicProfileRepository
-  
+  include Clean
+  include DataExtractor
+
+  def load_data(file_data)
+    contents = DataExtractor.extract(file_data, :economic_profile)
+    contents.each do |key, value|
+      contents_control(key, value)
+    end
+  end
+
+  def contents_control(label, contents)
+    case label
+    when :median_household_income
+      require "pry"; binding.pry
+      puts "median household income!"
+    when :children_in_poverty
+      puts "children_in_poverty!"
+    when :free_or_reduced_price_lunch
+      puts "free lunch!"
+    when :title_i
+      puts "title one!"
+    end
+  end
+
+
 end
--- test/economic_profile_repository_test.rb
+++ test/economic_profile_repository_test.rb
@@ -2,8 +2,17 @@ require_relative 'test_helper'
 require_relative '../lib/economic_profile_repository'
 
 class EconomicProfileRepositoryTest < MiniTest::Test
+  def setup
+    @epr = EconomicProfileRepository.new
+    @epr.load_data({
+        :economic_profile => {
+          :median_household_income => "./data/Median household income.csv",
+          :children_in_poverty => "./data/School-aged children in poverty.csv",
+          :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+          :title_i => "./data/Title I students.csv"
+          }})
+  end
   def test_it_exists
-    epr = EconomicProfileRepository.new
-    assert_instance_of EconomicProfileRepository, epr 
+    assert_instance_of EconomicProfileRepository, @epr
   end
 end
